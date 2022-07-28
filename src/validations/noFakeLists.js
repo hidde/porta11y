0107d8a145dee1dd83294d8da66d482e8debbef1
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-07-28 13:43:52 +0200

    Remove empty lines

--- src/validations/noFakeLists.js
+++ src/validations/noFakeLists.js
@@ -1,29 +1,31 @@
-
 /**
  * @param {Block[]} blocks - Portable Text AST
  * @param {Object} [options = {}] - Options for the validation
  * @param {RegExp} [options.regex = /\n\s*[-*+–—]/] - Validation regex
  * @param {String} [options.message = "This looks like a list, but it is plain text. Use the bulleted list option."] - Message to display when the validation triggers
  */
-
 function noFakeLists(blocks, options = {}) {
-  const regex = options.regex || /\n\s*[-*+–—]/;
-  const fakeListBlocks = (blocks || []).filter(
-    block =>
-      block._type === 'block' &&
-      block.children.some(blockPart =>
-        blockPart._type === 'span' &&
-        regex.test(blockPart.text)
+  const regex = options.regex || /\n\s*[-*+–—]/
+  const fakeListBlocks =
+    (blocks || [])
+      .filter(
+        block =>
+          block._type === 'block' &&
+          block.children.some(
+            blockPart =>
+              blockPart._type === 'span' && regex.test(blockPart.text)
+          )
       )
-  ).map(
-    (block, index) => [{ _key: block._key }] || [index]
-  ) || [];
+      .map((block, index) => [{ _key: block._key }] || [index]) || []
 
-  return fakeListBlocks.length === 0 || {
-        message: options.message || 'This looks like a list, but it is plain text. Use the bulleted list option.',
-        paths: fakeListBlocks
-      }
+  return (
+    fakeListBlocks.length === 0 || {
+      message:
+        options.message ||
+        'This looks like a list, but it is plain text. Use the bulleted list option.',
+      paths: fakeListBlocks,
+    }
+  )
 }
 
 export default noFakeLists
-
