8aee019f7201c5d6eaa433e32626eacdeaf8acea
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-08-01 10:16:49 +0200

    Move decorators in their own folder

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export { default as lang } from './annotations/lang'
-export { default as sup } from './annotations/sup'
+
+export { default as sup } from './decorators/sup'
+
 export { default as noFakeLists } from './validations/noFakeLists'
